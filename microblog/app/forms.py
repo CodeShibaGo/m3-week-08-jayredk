6a962843f5252ec72a5ebfa04c1a060657cd519a
Jay <kred110396@gmail.com>
2024-03-01 17:07:15 +0800

    feat: add mvp for i18n in spanish

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_babel import lazy_gettext as _l
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Length
 import sqlalchemy as sa
@@ -6,7 +7,7 @@ from app import db
 from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    username = StringField(_l('Username'), validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import current_user, login_user, logout_user, login_required
+from flask_babel import _, get_locale
 import sqlalchemy as sa
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
@@ -16,6 +17,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
+    g.locale = str(get_locale())
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
